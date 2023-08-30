6e99b2832895e5220902510223480b853445faab
dhanush-vemulapalli10 <113177233+dhanushvemulapalli@users.noreply.github.com>
2023-08-30 12:01:55 +0530

    Updated util.py analysed the code and added comments
    Added comments for util.py till queue fontier from top

--- util.py
+++ util.py
@@ -4,20 +4,24 @@ class Node():
         self.parent = parent
         self.action = action
 
-
+#code for the stack fontier
 class StackFrontier():
     def __init__(self):
         self.frontier = []
 
+    #Function for appending a node into the stack fontier
     def add(self, node):
         self.frontier.append(node)
 
+    #Checks id there are any state present in the fontier of the data structure
     def contains_state(self, state):
         return any(node.state == state for node in self.frontier)
 
+    #Checks if the fontier is empty
     def empty(self):
         return len(self.frontier) == 0
 
+    #Removes the last node from the fontier using the empty variable
     def remove(self):
         if self.empty():
             raise Exception("empty frontier")
